8087c3858baa874c65f560143c7fe9c174f8a160
Ali Raza <coder.raza@gmail.com>
2025-10-22 06:17:43 +0500

    Fixed Module Name of Mollie Official Module

--- models/payment_transaction.py
+++ models/payment_transaction.py
@@ -1,23 +1,14 @@
-import logging
-from odoo import models, api
-
-_logger = logging.getLogger(__name__)
+from odoo import models
 
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
-    @api.model
-    def _mollie_form_feedback(self, data):
-        """Called when Mollie sends a webhook or payment confirmation."""
-        _logger.info("💳 Mollie webhook received: %s", data)
-        result = super()._mollie_form_feedback(data)
-
-        # Find related partner
-        tx = self.search([('reference', '=', data.get('ref'))])
-        if tx and tx.partner_id:
-            _logger.info(f"🔁 Triggering Mollie mandate sync for partner {tx.partner_id.name}")
-            tx.partner_id.fetch_mollie_mandate()
-        else:
-            _logger.warning("⚠️ No transaction or partner found for Mollie webhook data: %s", data)
-
-        return result
+    def _set_done(self):
+        res = super()._set_done()
+        if self.acquirer_id.provider == 'mollie' and self.sale_order_ids:
+            for order in self.sale_order_ids:
+                order.mollie_transaction_id = self.acquirer_reference
+                order.mollie_payment_status = 'paid'
+                if self.acquirer_reference and self.mandate_id:
+                    order.mollie_mandate_id = self.mandate_id
+        return res
