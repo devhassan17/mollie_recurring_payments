fa4be31fb29c2ddb852157aee817506a0bf35276
devhassan17 <156808621+devhassan17@users.noreply.github.com>
2026-02-04 12:46:58 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mollie Subscription Renewals Dashboard',
-    'version': '18.0.1.6.0',
+    'version': '18.0.1.7.0',
     'category': 'Payment',
     'summary': 'Track Odoo subscription renewals charged via Mollie (Paid/Unpaid) with a dashboard',
     'description': """
