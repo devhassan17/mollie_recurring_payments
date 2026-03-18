5ddb3cd874476d138281867b627be2da1b1696dd
Ali Hassan <156808621+devhassan17@users.noreply.github.com>
2026-03-18 15:38:53 +0500

    fix: Remove access rights for the mollie.subscription.cron model.

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Mollie Subscription Renewals Dashboard & Recurring Payments',
-    'version': '18.0.4.3.0',
+    'version': '18.0.4.4.0',
     'category': 'Payment',
     'author': 'Managemyweb.co',
     'website': 'https://managemyweb.co',
